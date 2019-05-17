e5745df5dd8309d7ff5cec6b6f72026ef6d49742
Dimitar Topuzov <dtopuzov@gmail.com>
2019-05-17 10:54:54 +0300

    chore: remove comments

--- app/network/network-page.ts
+++ app/network/network-page.ts
@@ -4,23 +4,7 @@ import * as http from "tns-core-modules/http";
 
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
     let page = <Page>args.object;
-
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and TypeScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
     page.bindingContext = fromObjectRecursive({
         imageSrc: "",
         getNoBody() {
